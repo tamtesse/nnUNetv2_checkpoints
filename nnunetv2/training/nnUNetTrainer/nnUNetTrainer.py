fd20630ee7ba9d47f1ec4de6521f47a3256b3054
NataliaAlves13 <50765571+NataliaAlves13@users.noreply.github.com>
2024-04-16 19:09:54 +0100

    save checkpoints in individual files

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -1059,6 +1059,7 @@ class nnUNetTrainer(object):
         # handling periodic checkpointing
         current_epoch = self.current_epoch
         if (current_epoch + 1) % self.save_every == 0 and current_epoch != (self.num_epochs - 1):
+            self.save_checkpoint(join(self.output_folder, ('checkpoint_'+str(current_epoch+1)+'.pth')))
             self.save_checkpoint(join(self.output_folder, 'checkpoint_latest.pth'))
 
         # handle 'best' checkpointing. ema_fg_dice is computed by the logger and can be accessed like this
